644bb654889218bf0eb092796518c845703ff58d
张益铭 <zhangyiming@tiamaes.com>
2021-03-30 15:06:40 +0800

    fix: build error

--- src/views/components/button/index.vue
+++ src/views/components/button/index.vue
@@ -1,19 +1,27 @@
 <template>
-  <iframe src="./button.html" frameborder="0" class="iframe"></iframe>
+  <iframe
+    :src="url"
+    frameborder="0"
+    class="iframe"
+  ></iframe>
 </template>
 
 <script lang='ts'>
+import { ref } from 'vue';
 export default {
   setup() {
-    return {};
+    const url = ref(process.env.NODE_ENV === 'production' ? '/manages/html/button.html' : '/html/button.html');
+    return {
+      url
+    };
   },
 };
 </script>
 
 <style scoped>
 .iframe {
-  width:98%;
-  height:90vh;
+  width: 98%;
+  height: 90vh;
   border-radius: 6px;
 }
 </style>
