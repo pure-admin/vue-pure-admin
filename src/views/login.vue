84432c8bc5ef85c89daba479c1e924490b4eb33b
xiaoxian521 <1923740402@qq.com>
2021-03-31 15:27:07 +0800

    fix: shield verification code

--- src/views/login.vue
+++ src/views/login.vue
@@ -66,7 +66,7 @@ export default {
     };
 
     onBeforeMount(() => {
-      refreshGetVerify();
+      // refreshGetVerify();
     });
 
     return {
--- src/views/register.vue
+++ src/views/register.vue
@@ -59,7 +59,7 @@ export default {
     };
 
     onBeforeMount(() => {
-      refreshGetVerify();
+      // refreshGetVerify();
     });
 
     return {
