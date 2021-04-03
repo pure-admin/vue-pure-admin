42dfb536bd4839168c3b96c18938be67c5ef7328
xiaoxian521 <1923740402@qq.com>
2021-04-03 15:04:28 +0800

    feat: add countTo components

---
+++ src/utils/propTypes.ts
@@ -0,0 +1,33 @@
+import { CSSProperties, VNodeChild } from 'vue'
+import { createTypes, VueTypeValidableDef, VueTypesInterface } from 'vue-types'
+
+export type VueNode = VNodeChild | JSX.Element
+
+type PropTypes = VueTypesInterface & {
+  readonly style: VueTypeValidableDef<CSSProperties>
+  readonly VNodeChild: VueTypeValidableDef<VueNode>
+}
+
+const propTypes = createTypes({
+  func: undefined,
+  bool: undefined,
+  string: undefined,
+  number: undefined,
+  object: undefined,
+  integer: undefined,
+}) as PropTypes
+
+propTypes.extend([
+  {
+    name: 'style',
+    getter: true,
+    type: [String, Object],
+    default: undefined,
+  },
+  {
+    name: 'VNodeChild',
+    getter: true,
+    type: undefined,
+  }
+])
+export { propTypes }
--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -2,16 +2,19 @@
   <div class="welcome">
     <!-- <a title="欢迎Star" href="https://github.com/xiaoxian521/CURD-TS" target="_blank">点击打开仓库地址</a> -->
     <flop />
+    <CountTo prefix="$" :startVal="1" :endVal="200" />
   </div>
 </template>
 
 <script lang='ts'>
-import flop from "../components/flop/index.vue"
+import flop from "../components/flop/index.vue";
+import CountTo from "../components/countTo/src/index.vue";
 export default {
   name: "welcome",
   components: {
-    flop
-  },
+    flop,
+    CountTo
+  }
 };
 </script>
 
