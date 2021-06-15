f8fde9107bd85e4d848bfe846693e9b4b75dcb04
xiaoxian521 <1923740402@qq.com>
2021-06-15 16:55:15 +0800

    chore: update deps

--- src/utils/http/config.ts
+++ src/utils/http/config.ts
@@ -1,31 +1,31 @@
-import { AxiosRequestConfig } from "axios"
-import { excludeProps } from "./utils"
+import { AxiosRequestConfig } from "axios";
+import { excludeProps } from "./utils";
 /**
  * 默认配置
  */
 export const defaultConfig: AxiosRequestConfig = {
-  baseURL: '/api',
+  baseURL: "",
   timeout: 10000, //10秒超时
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest"
+    "X-Requested-With": "XMLHttpRequest",
   },
-}
+};
 
 export function genConfig(config?: AxiosRequestConfig): AxiosRequestConfig {
   if (!config) {
-    return defaultConfig
+    return defaultConfig;
   }
 
-  const { headers } = config
+  const { headers } = config;
   if (headers && typeof headers === "object") {
     defaultConfig.headers = {
       ...defaultConfig.headers,
-      ...headers
-    }
+      ...headers,
+    };
   }
-  return { ...excludeProps(config!, "headers"), ...defaultConfig }
+  return { ...excludeProps(config!, "headers"), ...defaultConfig };
 }
 
-export const METHODS = ["post", "get", "put", "delete", "option", "patch"]
+export const METHODS = ["post", "get", "put", "delete", "option", "patch"];
