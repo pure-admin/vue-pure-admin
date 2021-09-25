2b31f3b0e19691b054be98a7284859b685c85cfb
xiaoxian521 <1923740402@qq.com>
2021-09-25 18:32:56 +0800

    fix: update

--- src/layout/components/index.ts
+++ src/layout/components/index.ts
@@ -1,4 +1,3 @@
-export { default as NavHeader } from "./Sidebar/Horizontal.vue";
 export { default as Sidebar } from "./Sidebar/index.vue";
 export { default as AppMain } from "./AppMain.vue";
 export { default as setting } from "./setting/index.vue";
