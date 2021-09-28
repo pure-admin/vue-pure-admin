cf94aa1d03bfa1104ca09205a58311aece6d9dea
黄兵 <1278032416@qq.com>
2021-09-28 14:00:01 +0800

    feat(storage): add new storage
    新增 database 和 cookie 存储

---
+++ src/utils/storage/cookie.ts
@@ -0,0 +1,53 @@
+import { loadEnv } from "@build/utils";
+import { merge } from "lodash";
+import tsCookies from "typescript-cookie/dist/src/compat";
+
+class Cookies {
+  private static env = loadEnv();
+  constructor() {}
+  /**
+   *  存储 cookie 值
+   * @param name
+   * @param value
+   * @param cookieSetting
+   */
+  set(name = "default", value = "", cookieSetting = {}) {
+    const currentCookieSetting = {
+      expires: 1
+    };
+    merge(currentCookieSetting, cookieSetting);
+    tsCookies.set(
+      `${Cookies.env.VITE_TITLE}-${Cookies.env.VITE_VERSION}-${name}`,
+      value,
+      currentCookieSetting
+    );
+  }
+  /**
+   * 拿到 cookie 值
+   * @param name
+   * @returns
+   */
+  get(name = "default"): any {
+    return tsCookies.get(
+      `${Cookies.env.VITE_TITLE}-${Cookies.env.VITE_VERSION}-${name}`
+    );
+  }
+  /**
+   * 拿到 cookie 全部的值
+   * @returns
+   */
+  getAll(): any {
+    return tsCookies.get();
+  }
+  /**
+   * 删除 cookie
+   * @param name
+   */
+  remove(name = "default") {
+    tsCookies.remove(
+      `${Cookies.env.VITE_TITLE}-${Cookies.env.VITE_VERSION}-${name}`
+    );
+  }
+}
+
+export const cookies = new Cookies();
--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ const pathResolve = (dir: string): any => {
 
 const alias: Record<string, string> = {
   "/@": pathResolve("src"),
+  "@build": pathResolve("build"),
   //解决开发环境下的警告 You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
   "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js"
 };
