1687097e6301e41379dcfc65aaa724e473affa12
xiaoxian521 <1923740402@qq.com>
2021-10-14 17:36:16 +0800

    fix: element-plus locale config

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
-<script setup lang="ts">
-import { getCurrentInstance } from "vue";
-import { ElConfigProvider } from "element-plus";
-import zhCn from "element-plus/lib/locale/lang/zh-cn";
-import en from "element-plus/lib/locale/lang/en";
-
-let locale: string =
-  getCurrentInstance().appContext.config.globalProperties.$storage?.locale
-    ?.locale;
-
-let currentLocale = () => {
-  switch (locale) {
-    case "zh":
-      return zhCn;
-    case "en":
-      return en;
-  }
-};
-</script>
-
 <template>
-  <el-config-provider :locale="currentLocale()">
+  <el-config-provider :locale="currentLocale">
     <router-view />
   </el-config-provider>
 </template>
+
+<script lang="ts">
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+import en from "element-plus/lib/locale/lang/en";
+export default {
+  name: "app",
+  components: {
+    [ElConfigProvider.name]: ElConfigProvider
+  },
+  computed: {
+    // eslint-disable-next-line vue/return-in-computed-property
+    currentLocale() {
+      switch (this.$storage.locale?.locale) {
+        case "zh":
+          return zhCn;
+        case "en":
+          return en;
+      }
+    }
+  }
+};
+</script>
