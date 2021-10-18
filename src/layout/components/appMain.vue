9b4cbaae6306073bad2eb59f2dd60654ab10b711
zhuoyue03007 <zhuoyue03007@163.com>
2021-10-18 14:49:53 +0800

    Update appMain.vue

--- src/layout/components/appMain.vue
+++ src/layout/components/appMain.vue
@@ -16,7 +16,7 @@ const transition = computed(() => {
 <template>
   <section class="app-main">
     <el-scrollbar>
-      <router-view1>
+      <router-view>
         <template #default="{ Component, route }">
           <transition
             :name="
