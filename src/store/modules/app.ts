c18dbea3cdf5eb25a160f97bd2a205a4c18d7c12
xiaoxian521 <1923740402@qq.com>
2021-11-04 20:34:59 +0800

    fix: update

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -24,8 +24,7 @@ export const useAppStore = defineStore({
     },
     // 这里的layout用于监听容器拖拉后恢复对应的导航模式
     layout:
-      storageLocal.getItem("responsive-layout")?.layout?.layout ??
-      getConfig().Layout,
+      storageLocal.getItem("responsive-layout")?.layout ?? getConfig().Layout,
     device: deviceDetection() ? "mobile" : "desktop"
   }),
   getters: {
