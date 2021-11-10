b45a10b98f5d6cbdff5bffb5d4ec60a322c9b828
xiaoxian521 <1923740402@qq.com>
2021-11-10 09:55:01 +0800

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -110,11 +110,14 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
           // 在生产模式是否抽取独立的主题css文件，extract为true以下属性有效
           extract: true,
           // 独立主题css文件的输出路径，默认取 viteConfig.build.assetsDir 相对于 (viteConfig.build.outDir)
-          outputDir: "",
+          outputDir:
+            process.env.NODE_ENV === "production"
+              ? "/manages/assets/"
+              : VITE_PUBLIC_PATH,
           // 会选取defaultScopeName对应的主题css文件在html添加link
-          themeLinkTagId: "body",
+          themeLinkTagId: "head",
           // "head"||"head-prepend" || "body" ||"body-prepend"
-          themeLinkTagInjectTo: "body",
+          themeLinkTagInjectTo: "head",
           // 是否对抽取的css文件内对应scopeName的权重类名移除
           removeCssScopeName: false,
           // 可以自定义css文件名称的函数
