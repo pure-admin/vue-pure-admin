724c6bc85a7675bd9c5ef70b2d36e89e81e0e14d
realMess <chuangyi110@163.com>
2021-11-23 10:39:27 +0800

    fix: json format

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
   "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": [
-    "prettier --write--parser json"
+    "prettier --write --parser json"
   ],
   "package.json": ["prettier --write"],
   "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
