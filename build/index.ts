a0c54a6ac92d11d498dd570e3f3b8622b8b35465
xiaoxian521 <1923740402@qq.com>
2021-12-13 17:39:13 +0800

    fix: types

--- build/index.ts
+++ build/index.ts
@@ -5,7 +5,8 @@ const warpperEnv = (envConf: Recordable): ViteEnv => {
     VITE_PORT: 8848,
     VITE_PUBLIC_PATH: "",
     VITE_PROXY_DOMAIN: "",
-    VITE_PROXY_DOMAIN_REAL: ""
+    VITE_PROXY_DOMAIN_REAL: "",
+    VITE_ROUTER_HISTORY: ""
   };
 
   for (const envName of Object.keys(envConf)) {
