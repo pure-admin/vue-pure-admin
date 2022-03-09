c9026a45cc485b5a32f5340481c2d12d610060af
一万 <52823142+Ten-K@users.noreply.github.com>
2022-03-09 13:54:16 +0800

    feat: menu search (#209)
    * feat: menu search
    
    * style(layout): 剔除windcss依赖

---
+++ src/layout/components/search/components/SearchFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="search-footer">
+    <span class="search-footer-item">
+      <enterOutlined class="icon" />
+      确认
+    </span>
+    <span class="search-footer-item">
+      <IconifyIconOffline icon="arrow-up-line" class="icon" />
+      <IconifyIconOffline icon="arrow-down-line" class="icon" />
+      切换
+    </span>
+    <span class="search-footer-item">
+      <mdiKeyboardEsc class="icon" />
+      关闭
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import enterOutlined from "/@/assets/svg/enter_outlined.svg?component";
+import mdiKeyboardEsc from "/@/assets/svg/mdi_keyboard_esc.svg?component";
+</script>
+<style lang="scss" scoped>
+.search-footer {
+  display: flex;
+  color: #333;
+
+  .search-footer-item {
+    display: flex;
+    align-items: center;
+    margin-right: 14px;
+  }
+
+  .icon {
+    padding: 2px;
+    margin-right: 3px;
+    font-size: 20px;
+    box-shadow: inset 0 -2px #cdcde6, inset 0 0 1px 1px #fff,
+      0 1px 2px 1px #1e235a66;
+  }
+}
+</style>
---
+++ src/layout/hooks/useBoolean.ts
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+
+export default function useBoolean(initValue = false) {
+  const bool = ref(initValue);
+
+  function setBool(value: boolean) {
+    bool.value = value;
+  }
+  function setTrue() {
+    setBool(true);
+  }
+  function setFalse() {
+    setBool(false);
+  }
+  function toggle() {
+    setBool(!bool.value);
+  }
+
+  return {
+    bool,
+    setBool,
+    setTrue,
+    setFalse,
+    toggle
+  };
+}
