ae57e42612bdc4e9eece24f1c229fe33377c8390
xiaoxian521 <1923740402@qq.com>
2022-03-17 19:00:25 +0800

    perf: router rank

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -22,6 +22,13 @@ import { getAsyncRoutes } from "/@/api/routes";
 
 // 按照路由中meta下的rank等级升序来排序路由
 function ascending(arr: any[]) {
+  arr.forEach(v => {
+    if (v?.meta?.rank === 0) {
+      if (v.name !== "home" && v.path !== "/") {
+        console.warn("rank only the home page can be 0");
+      }
+    }
+  });
   return arr.sort(
     (a: { meta: { rank: number } }, b: { meta: { rank: number } }) => {
       return a?.meta?.rank - b?.meta?.rank;
