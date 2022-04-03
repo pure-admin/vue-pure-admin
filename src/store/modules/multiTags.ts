5e8723a031923e79f507e5a17151d3bd88a51523
xiaoxian521 <1923740402@qq.com>
2022-04-03 15:30:45 +0800

    feat: 导出关闭某个标签的hooks

--- src/store/modules/multiTags.ts
+++ src/store/modules/multiTags.ts
@@ -89,7 +89,13 @@ export const useMultiTagsStore = defineStore({
           }
           break;
         case "splice":
-          this.multiTags.splice(position?.startIndex, position?.length);
+          if (!position) {
+            const index = this.multiTags.findIndex(v => v.path === value);
+            if (index === -1) return;
+            this.multiTags.splice(index, 1);
+          } else {
+            this.multiTags.splice(position?.startIndex, position?.length);
+          }
           this.tagsCache(this.multiTags);
           return this.multiTags;
         case "slice":
