c4ebacedfee944250ac358696914240768a60d29
xiaoxian521 <1923740402@qq.com>
2022-04-10 23:54:14 +0800

    feat: 新增部门管理demo

--- mock/system.ts
+++ mock/system.ts
@@ -51,5 +51,137 @@ export default [
         msg: ""
       };
     }
+  },
+  {
+    url: "/dept",
+    method: "post",
+    response: () => {
+      return {
+        code: 0,
+        data: [
+          {
+            name: "pure-admin",
+            parentId: 0,
+            sort: 0,
+            leaderUserId: 1,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 100,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "深圳总公司",
+            parentId: 100,
+            sort: 1,
+            leaderUserId: 104,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 101,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "研发部门",
+            parentId: 101,
+            sort: 1,
+            leaderUserId: 104,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 103,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "市场部门",
+            parentId: 102,
+            sort: 1,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 108,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "长沙分公司",
+            parentId: 100,
+            sort: 2,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 102,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "市场部门",
+            parentId: 101,
+            sort: 2,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 1,
+            id: 104,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "财务部门",
+            parentId: 102,
+            sort: 2,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 109,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "测试部门",
+            parentId: 101,
+            sort: 3,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 105,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "财务部门",
+            parentId: 101,
+            sort: 4,
+            leaderUserId: 103,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 1,
+            id: 106,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          },
+          {
+            name: "运维部门",
+            parentId: 101,
+            sort: 5,
+            leaderUserId: null,
+            phone: "15888888888",
+            email: "ry@qq.com",
+            status: 0,
+            id: 107,
+            createTime: 1609837427000,
+            remark: "备注、备注、备注、备注、备注、备注、备注"
+          }
+        ],
+        msg: ""
+      };
+    }
   }
 ] as MockMethod[];
--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -117,3 +117,59 @@ export function appendFieldByUniqueId(
   }
   return menuTree;
 }
+
+/**
+ * 构造树型结构数据
+ * @param {*} data 数据源
+ * @param {*} id id字段 默认 'id'
+ * @param {*} parentId 父节点字段 默认 'parentId'
+ * @param {*} children 孩子节点字段 默认 'children'
+ */
+export function handleTree(
+  data,
+  id?: string,
+  parentId?: string,
+  children?: string
+) {
+  const config = {
+    id: id || "id",
+    parentId: parentId || "parentId",
+    childrenList: children || "children"
+  };
+
+  const childrenListMap = {};
+  const nodeIds = {};
+  const tree = [];
+
+  for (const d of data) {
+    const parentId = d[config.parentId];
+    if (childrenListMap[parentId] == null) {
+      childrenListMap[parentId] = [];
+    }
+    nodeIds[d[config.id]] = d;
+    childrenListMap[parentId].push(d);
+  }
+
+  for (const d of data) {
+    const parentId = d[config.parentId];
+    if (nodeIds[parentId] == null) {
+      tree.push(d);
+    }
+  }
+
+  for (const t of tree) {
+    adaptToChildrenList(t);
+  }
+
+  function adaptToChildrenList(o) {
+    if (childrenListMap[o[config.id]] !== null) {
+      o[config.childrenList] = childrenListMap[o[config.id]];
+    }
+    if (o[config.childrenList]) {
+      for (const c of o[config.childrenList]) {
+        adaptToChildrenList(c);
+      }
+    }
+  }
+  return tree;
+}
