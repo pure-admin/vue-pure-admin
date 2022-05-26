7e5fc98d570b6b7a114182c1d21bff85d0faeb78
lrl <742798240@qq.com>
2022-05-26 21:12:57 +0800

    fix: update

--- src/views/able/pdf.vue
+++ src/views/able/pdf.vue
@@ -11,11 +11,19 @@ const { t } = useI18n();
 
 const source =
   "https://pure-admin-doc.vercel.app/pdf/Cookie%E5%92%8CSession%E5%8C%BA%E5%88%AB%E7%94%A8%E6%B3%95.pdf";
+const pdfRef = ref();
 let loading = ref(true);
-const pdfRef = ref<HTMLElement | null>(null);
+let showAllPages = ref(false);
+let pageCount = ref(1);
+let currentPage = ref(1);
 
-const hideLoading = () => {
+const handleDocumentRender = () => {
   loading.value = false;
+  pageCount.value = pdfRef.value.pageCount;
+};
+
+const showAllPagesChange = () => {
+  currentPage.value = showAllPages.value ? null : 1;
 };
 </script>
 
@@ -39,12 +47,32 @@ const hideLoading = () => {
       v-loading="loading"
       :element-loading-text="t('status.hsLoad')"
     >
+      <div class="flex justify-between items-center h-9">
+        <div v-if="showAllPages" class="font-medium ml-1.25 text-xl">
+          {{ pageCount }} page(s)
+        </div>
+        <div v-else>
+          <el-pagination
+            background
+            layout="prev, slot, next"
+            v-model:current-page="currentPage"
+            :page-size="1"
+            :total="pageCount"
+          >
+            {{ currentPage }} / {{ pageCount }}
+          </el-pagination>
+        </div>
+        <el-checkbox v-model="showAllPages" @change="showAllPagesChange">
+          Show all pages
+        </el-checkbox>
+      </div>
       <el-scrollbar>
         <vue-pdf-embed
           class="h-full container overflow-auto"
           ref="pdfRef"
+          :page="currentPage"
           :source="source"
-          @rendered="hideLoading"
+          @rendered="handleDocumentRender"
         />
       </el-scrollbar>
     </div>
