91576bd7ddca1a8d83d460325160323f1970d356
xiaoxian521 <1923740402@qq.com>
2022-06-24 14:12:56 +0800

    chore: update `@pureadmin/descriptions`

--- src/views/result/fail.vue
+++ src/views/result/fail.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { useColumns } from "./columns";
 defineOptions({
   name: "Fail"
 });
+
+const { columns } = useColumns();
 </script>
 
 <template>
@@ -20,61 +23,12 @@ defineOptions({
         <el-button type="primary">返回修改</el-button>
       </template>
     </el-result>
-    <el-descriptions
-      :column="1"
+    <PureDescriptions
+      :columns="columns"
       title="您提交的内容有如下错误："
       style="background: rgb(250, 250, 250)"
       class="p-6 ml-10 mr-10"
-    >
-      <el-descriptions-item>
-        <span class="flex items-center -mt-6">
-          <IconifyIconOffline
-            icon="close-circle-line"
-            color="#F56C6C"
-            width="18px"
-            height="18px"
-          />
-          <span class="ml-1 mr-4">您的账户已被冻结</span>
-          <a
-            href="javascript:void(0);"
-            class="flex items-center"
-            style="color: var(--el-color-primary)"
-          >
-            立即解冻
-            <IconifyIconOffline
-              icon="arrow-right-s-line"
-              color="var(--el-color-primary)"
-              width="18px"
-              height="18px"
-            />
-          </a>
-        </span>
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <span class="flex items-center -mt-8">
-          <IconifyIconOffline
-            icon="close-circle-line"
-            color="#F56C6C"
-            width="18px"
-            height="18px"
-          />
-          <span class="ml-1 mr-4">您的账户还不具备申请资格</span>
-          <a
-            href="javascript:void(0);"
-            class="flex items-center"
-            style="color: var(--el-color-primary)"
-          >
-            立即升级
-            <IconifyIconOffline
-              icon="arrow-right-s-line"
-              color="var(--el-color-primary)"
-              width="18px"
-              height="18px"
-            />
-          </a>
-        </span>
-      </el-descriptions-item>
-    </el-descriptions>
+    />
   </el-card>
 </template>
 
