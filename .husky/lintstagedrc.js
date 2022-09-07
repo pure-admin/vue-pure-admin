392105e8202f2280f0d86873d742eeae362abe80
xiaoxian521 <1923740402@qq.com>
2022-09-07 21:59:03 +0800

    chore: use pnpm `shell-emulator` instead of `cross-env`

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "{!(package)*.json,.!(browserslist)*rc}": ["prettier --write--parser json"],
+  "{!(package)*.json}": ["prettier --write--parser json"],
   "package.json": ["prettier --write"],
   "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
   "*.{vue,css,scss,postcss,less}": ["stylelint --fix", "prettier --write"],
