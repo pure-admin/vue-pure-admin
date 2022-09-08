594f9b98abe71b1a452088c4fee009efd737ca25
xiaoxian521 <1923740402@qq.com>
2022-09-08 17:23:48 +0800

    feat: 添加`cssnano`，打包时压缩`css`体积

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {})
   }
 };
