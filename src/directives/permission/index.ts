c32756b5287b874ae61d33f55ec31f5506a61b35
xiaoxian521 <1923740402@qq.com>
2022-10-21 13:29:05 +0800

    chore: update

--- src/directives/permission/index.ts
+++ src/directives/permission/index.ts
@@ -1,28 +1,29 @@
-import { isEqual } from "@pureadmin/utils";
+import router from "/@/router";
 import { Directive, type DirectiveBinding } from "vue";
-import { usePermissionStoreHook } from "/@/store/modules/permission";
+import { isString, isIncludeAllChildren } from "@pureadmin/utils";
 
-export const auth: Directive = {
+export const permission: Directive = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     const { value } = binding;
     if (value) {
-      const authRoles = value;
+      /** 从当前路由的`meta`字段里获取按钮级别的所有自定义`code`值 */
+      const metaPermissions = router.currentRoute.value.meta
+        .permissions as Array<string>;
 
       /** 兼容传`字符串`或`字符串数组`两种写法
-       *  字符串写法例子：v-auth="'btn.add'"
-       *  字符串数组写法例子：v-auth="['btn.add','btn.edit']"
+       *  字符串写法例子：v-permission="'btn.add'"
+       *  字符串数组写法例子：v-permission="['btn.add','btn.edit']"
        */
-      const hasAuth = isEqual(
-        usePermissionStoreHook().permissions,
-        [authRoles].flat(Infinity)
-      );
+      const hasPermissions = isString(value)
+        ? metaPermissions.includes(value)
+        : isIncludeAllChildren(value, metaPermissions);
 
-      if (!hasAuth) {
+      if (!hasPermissions) {
         el.parentNode.removeChild(el);
       }
     } else {
       throw new Error(
-        "need permissions! Like v-auth=\"['btn.add','btn.edit']\""
+        "need permissions! Like v-permission=\"['btn.add','btn.edit']\""
       );
     }
   }
