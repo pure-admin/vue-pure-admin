44e3ff2bb869a5a7f0719ce716fb86b429023680
xiaoxian521 <1923740402@qq.com>
2022-10-24 18:39:16 +0800

    chore: update

--- types/index.ts
+++ types/index.ts
@@ -74,6 +74,8 @@ export interface RouteChildrenConfigsTable {
     showLink?: boolean;
     /** 是否显示父级菜单 `可选` */
     showParent?: boolean;
+    /** 页面级别权限设置 `可选` */
+    roles?: Array<string>;
     /** 按钮级别权限设置 `可选` */
     auths?: Array<string>;
     /** 路由组件缓存（开启 `true`、关闭 `false`）`可选` */
