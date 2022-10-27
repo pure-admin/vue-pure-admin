aa83ea83fa377033367e663f99ddfab6cb35b8eb
xiaoxian521 <1923740402@qq.com>
2022-10-27 15:51:59 +0800

    feat: 静态资源分类打包

--- vite.config.ts
+++ vite.config.ts
@@ -69,7 +69,18 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
     build: {
       sourcemap: false,
       // 消除打包大小超过500kb警告
-      chunkSizeWarningLimit: 4000
+      chunkSizeWarningLimit: 4000,
+      rollupOptions: {
+        input: {
+          index: pathResolve("index.html")
+        },
+        // 静态资源分类打包
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]"
+        }
+      }
     },
     define: {
       __INTLIFY_PROD_DEVTOOLS__: false,
