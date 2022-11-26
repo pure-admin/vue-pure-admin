667ef918fc50029b23a01d754917e6ae0df45c2f
xiaoxian521 <1923740402@qq.com>
2022-11-27 02:25:09 +0800

    perf: 无需安装 `@vue/runtime-core`，兼容 `element-plus` 的组件 `volar` 提示

--- types/global.d.ts
+++ types/global.d.ts
@@ -265,15 +265,3 @@ declare global {
     $config: ServerConfigs;
   }
 }
-
-declare module "vue" {
-  /**
-   * 自定义全局组件获得 Volar 提示（自定义的全局组件需要在这里声明下才能获得 Volar 类型提示哦）
-   */
-  export interface GlobalComponents {
-    IconifyIconOffline: typeof import("../src/components/ReIcon")["IconifyIconOffline"];
-    IconifyIconOnline: typeof import("../src/components/ReIcon")["IconifyIconOnline"];
-    FontIcon: typeof import("../src/components/ReIcon")["FontIcon"];
-    Auth: typeof import("../src/components/ReAuth")["Auth"];
-  }
-}
