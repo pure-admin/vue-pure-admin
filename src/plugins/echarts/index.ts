71d0ec8ba85e1f9e0e733186d140717346e715ca
xiaoxian521 <1923740402@qq.com>
2022-11-30 13:27:27 +0800

    chore: update

--- src/plugins/echarts/index.ts
+++ src/plugins/echarts/index.ts
@@ -1,6 +1,6 @@
 import type { App } from "vue";
 import * as echarts from "echarts/core";
-import { SVGRenderer } from "echarts/renderers";
+import { CanvasRenderer } from "echarts/renderers";
 import { PieChart, BarChart, LineChart } from "echarts/charts";
 import {
   GridComponent,
@@ -20,7 +20,7 @@ use([
   PieChart,
   BarChart,
   LineChart,
-  SVGRenderer,
+  CanvasRenderer,
   GridComponent,
   TitleComponent,
   LegendComponent,
@@ -41,6 +41,7 @@ use([
 /**
  * @description 按需引入echarts
  * @see {@link https://echarts.apache.org/handbook/zh/basics/import#%E6%8C%89%E9%9C%80%E5%BC%95%E5%85%A5-echarts-%E5%9B%BE%E8%A1%A8%E5%92%8C%E7%BB%84%E4%BB%B6}
+ * @see 温馨提示：必须将 `$echarts` 添加到全局 `globalProperties` ，为了配合 https://pure-admin-utils.netlify.app/hooks/useEcharts/useEcharts.html 使用
  */
 export function useEcharts(app: App) {
   app.config.globalProperties.$echarts = echarts;
