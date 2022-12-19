d36d5b09c518573f0541c3a12a6f86b9056cfd16
xiaoxian521 <1923740402@qq.com>
2022-12-19 10:42:26 +0800

    fix: 修复`tailwind.css`错误的引入方式导致`vite`的`hmr`慢的问题

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ import PureDescriptions from "@pureadmin/descriptions";
 import "./style/reset.scss";
 // 导入公共样式
 import "./style/index.scss";
+// 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
+import "./style/tailwind.css";
 import "element-plus/dist/index.css";
 // 导入字体图标
 import "./assets/iconfont/iconfont.js";
