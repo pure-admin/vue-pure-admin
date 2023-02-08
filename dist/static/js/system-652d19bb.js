585997dbb782b8730859c53fec28ef3c79f7ca8e
xiaoxian521 <1923740402@qq.com>
2023-02-09 00:25:06 +0800

    chore: update

---
+++ dist/static/js/system-652d19bb.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-84b6e66a.js";const r=t=>e.request("post","/user",{data:t}),o=t=>e.request("post","/role",{data:t}),p=t=>e.request("post","/dept",{data:t});export{o as a,r as b,p as g};
