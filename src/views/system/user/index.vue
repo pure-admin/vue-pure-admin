39833ce917dc8eea27fe2a097e1ad08dc9fffa64
xiaoxian521 <1923740402@qq.com>
2023-06-09 14:01:46 +0800

    perf: 系统管理中表格均改为自适应内容区高度，需将`@pureadmin/table`升级到最新版

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -39,7 +39,7 @@ const {
 <template>
   <div class="main">
     <tree class="w-[17%] float-left" />
-    <div class="float-right w-[81%]">
+    <div class="float-right w-[82%]">
       <el-form
         ref="formRef"
         :inline="true"
@@ -97,6 +97,7 @@ const {
         <template v-slot="{ size, dynamicColumns }">
           <pure-table
             border
+            adaptive
             align-whole="center"
             table-layout="auto"
             :loading="loading"
