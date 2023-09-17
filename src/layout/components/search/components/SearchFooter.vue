d49bdb0eb6dde35167e3ba29c8e6e34bdf6b2c08
xiaoxian521 <1923740402@qq.com>
2023-09-17 16:00:27 +0800

    chore: 升级代码规范风格相关库至最新

--- src/layout/components/search/components/SearchFooter.vue
+++ src/layout/components/search/components/SearchFooter.vue
@@ -50,7 +50,9 @@ const { device } = useNav();
     padding: 2px;
     margin-right: 3px;
     font-size: 20px;
-    box-shadow: inset 0 -2px #cdcde6, inset 0 0 1px 1px #fff,
+    box-shadow:
+      inset 0 -2px #cdcde6,
+      inset 0 0 1px 1px #fff,
       0 1px 2px 1px #1e235a66;
   }
 
