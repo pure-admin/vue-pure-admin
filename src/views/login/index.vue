73b5d3ec41705ae36e154dd21a8bdac61679a590
xiaoxian521 <1923740402@qq.com>
2023-09-18 20:22:26 +0800

    perf: 优化登录页`loading`判断

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -80,7 +80,8 @@ const onLogin = async (formEl: FormInstance | undefined) => {
               message("登录成功", { type: "success" });
             });
           }
-        });
+        })
+        .finally(() => (loading.value = false));
     } else {
       loading.value = false;
       return fields;
