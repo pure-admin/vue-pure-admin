d949ba02e0884e4d8c75cac6e4676ff00e0f2ffa
xiaoxian521 <1923740402@qq.com>
2023-10-20 10:58:20 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -37,8 +37,6 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
     },
     // 服务端渲染
     server: {
-      // 是否开启 https
-      https: false,
       // 端口号
       port: VITE_PORT,
       host: "0.0.0.0",
@@ -46,7 +44,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       proxy: {},
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
       warmup: {
-        clientFiles: ["./index.html", "./src/views/*", "./src/components/*"]
+        clientFiles: ["./index.html", "./src/{views,components}/*"]
       }
     },
     plugins: getPluginsList(command, VITE_CDN, VITE_COMPRESSION),
