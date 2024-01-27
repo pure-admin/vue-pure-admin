30076fe3adc40e821ad3cf1a85789b4ee1598b68
苗大 <ufo025174@gmail.com>
2024-01-28 00:17:30 +0800

    perf(ReText): tippy 引入方式改为Composition API

--- src/components/ReText/src/index.vue
+++ src/components/ReText/src/index.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup>
 import { useResizeObserver } from "@vueuse/core";
 import type { ElText } from "element-plus";
-import { PropType, computed, h, onMounted, ref, useAttrs, useSlots } from "vue";
-import { TippyOptions } from "vue-tippy";
+import {
+  PropType,
+  computed,
+  h,
+  onMounted,
+  ref,
+  useAttrs,
+  useSlots,
+  watchEffect
+} from "vue";
+import { TippyOptions, useTippy } from "vue-tippy";
 
 const props = defineProps({
   // 行数
@@ -17,31 +26,17 @@ const props = defineProps({
 const $attrs = useAttrs();
 const $slots = useSlots();
 
-const tippyVisible = ref(false);
 const textRef = ref<InstanceType<typeof ElText>>();
 
-const getTippyVisible = () => {
+const isTextEllipsis = () => {
   if (!props.lineClamp) {
     // 单行省略判断
-    tippyVisible.value =
-      textRef.value?.$el.scrollWidth > textRef.value?.$el.clientWidth;
+    return textRef.value?.$el.scrollWidth > textRef.value?.$el.clientWidth;
   } else {
     // 多行省略判断
-    tippyVisible.value =
-      textRef.value?.$el.scrollHeight > textRef.value?.$el.clientHeight;
+    return textRef.value?.$el.scrollHeight > textRef.value?.$el.clientHeight;
   }
 };
-
-const getTippyProps = computed(() => {
-  return tippyVisible.value
-    ? Object.assign(
-        {
-          content: h($slots.content || $slots.default)
-        },
-        props.tippyProps
-      )
-    : undefined;
-});
 
 const getTextProps = computed(() => {
   return Object.assign(
@@ -54,16 +49,30 @@ const getTextProps = computed(() => {
 });
 
 onMounted(() => {
-  getTippyVisible();
+  const { disable, enable, setProps } = useTippy(textRef.value?.$el, {});
 
+  // 监听 props 变化
+  watchEffect(() => {
+    setProps(
+      Object.assign(
+        {
+          content: h($slots.content || $slots.default)
+        },
+        props.tippyProps
+      )
+    );
+    isTextEllipsis() ? enable() : disable();
+  });
+
+  // 监听文本宽度变化
   useResizeObserver(textRef, () => {
-    getTippyVisible();
+    isTextEllipsis() ? enable() : disable();
   });
 });
 </script>
 
 <template>
-  <el-text v-bind="getTextProps" ref="textRef" v-tippy="getTippyProps">
+  <el-text v-bind="getTextProps" ref="textRef">
     <slot />
   </el-text>
 </template>
