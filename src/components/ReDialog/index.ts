4339ebc5f2c9d0e1474abb6b10a009b82805f50c
xiaoxian521 <1923740402@qq.com>
2024-03-26 23:44:42 +0800

    chore: udpate

--- src/components/ReDialog/index.ts
+++ src/components/ReDialog/index.ts
@@ -51,8 +51,8 @@ const closeAllDialog = () => {
 
 /** 千万别忘了在下面这三处引入并注册下，放心注册，不使用`addDialog`调用就不会被挂载
  * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L4
- * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L13
- * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L20
+ * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L12
+ * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L22
  */
 const ReDialog = withInstall(reDialog);
 
