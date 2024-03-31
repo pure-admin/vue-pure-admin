f601e86ebf278a6e165f8144eb979e4ae2606267
erkelost <1256029807@qq.com>
2024-03-31 14:25:15 +0800

    feat: add farm startup mode

--- build/utils.ts
+++ build/utils.ts
@@ -62,7 +62,6 @@ const alias: Record<string, string> = {
 /** 平台的名称、版本、运行所需的`node`和`pnpm`版本、依赖、最后构建时间的类型提示 */
 const __APP_INFO__ = {
   pkg: { name, version, engines, dependencies, devDependencies },
-  // lastBuildTime: "2024-03-24 14:39:02"
   lastBuildTime: dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss")
 };
 
