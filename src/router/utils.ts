27a0b6fda78933193a88845c83bc43922e3c8bab
xiaoxian521 <1923740402@qq.com>
2024-04-09 14:22:02 +0800

    chore: update

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -17,9 +17,9 @@ import {
   isIncludeAllChildren
 } from "@pureadmin/utils";
 import { getConfig } from "@/config";
-import type { menuType } from "@/layout/types";
 import { buildHierarchyTree } from "@/utils/tree";
 import { userKey, type DataInfo } from "@/utils/auth";
+import { type menuType, routerArrays } from "@/layout/types";
 import { useMultiTagsStoreHook } from "@/store/modules/multiTags";
 import { usePermissionStoreHook } from "@/store/modules/permission";
 const IFrame = () => import("@/layout/frameView.vue");
@@ -178,6 +178,10 @@ function handleAsyncRoutes(routeList) {
     );
     usePermissionStoreHook().handleWholeMenus(routeList);
   }
+  useMultiTagsStoreHook().handleTags("equal", [
+    ...routerArrays,
+    ...usePermissionStoreHook().flatteningRoutes.filter(v => v?.meta?.fixedTag)
+  ]);
   addPathMatch();
 }
 
