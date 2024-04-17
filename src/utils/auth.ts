9ad00d97ee96eb4fba5a638131c2fe46ed99eb41
fangcy <524738813@qq.com>
2024-04-17 18:17:39 +0800

    fix: 修复调用刷新`token`接口时，`refreshToken`参数为空的问题

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -40,7 +40,7 @@ export function getToken(): DataInfo<number> {
 /**
  * @description 设置`token`以及一些必要信息并采用无感刷新`token`方案
  * 无感刷新：后端返回`accessToken`（访问接口使用的`token`）、`refreshToken`（用于调用刷新`accessToken`的接口时所需的`token`，`refreshToken`的过期时间（比如30天）应大于`accessToken`的过期时间（比如2小时））、`expires`（`accessToken`的过期时间）
- * 将`accessToken`、`expires`这两条信息放在key值为authorized-token的cookie里（过期自动销毁）
+ * 将`accessToken`、`expires`、`refreshToken`这三条信息放在key值为authorized-token的cookie里（过期自动销毁）
  * 将`avatar`、`username`、`nickname`、`roles`、`refreshToken`、`expires`这六条信息放在key值为`user-info`的localStorage里（利用`multipleTabsKey`当浏览器完全关闭后自动销毁）
  */
 export function setToken(data: DataInfo<Date>) {
@@ -48,7 +48,7 @@ export function setToken(data: DataInfo<Date>) {
   const { accessToken, refreshToken } = data;
   const { isRemembered, loginDay } = useUserStoreHook();
   expires = new Date(data.expires).getTime(); // 如果后端直接设置时间戳，将此处代码改为expires = data.expires，然后把上面的DataInfo<Date>改成DataInfo<number>即可
-  const cookieString = JSON.stringify({ accessToken, expires });
+  const cookieString = JSON.stringify({ accessToken, expires, refreshToken });
 
   expires > 0
     ? Cookies.set(TokenKey, cookieString, {
