8934499349d1004174ebc28106d1f81d6e175a26
xiaoxian521 <1923740402@qq.com>
2024-05-14 09:32:40 +0800

    chore(deps): update

--- types/index.d.ts
+++ types/index.d.ts
@@ -75,6 +75,8 @@ interface ComponentElRef<T extends HTMLElement = HTMLDivElement> {
   $el: T;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function parseInt(s: string | number, radix?: number): number;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function parseFloat(string: string | number): number;
