bdb683f59af393305f8f30a2991f636c4e4eb131
xiaoxian521 <1923740402@qq.com>
2024-06-06 17:27:15 +0800

    chore: update

--- build/plugins.ts
+++ build/plugins.ts
@@ -5,6 +5,7 @@ import { viteBuildInfo } from "./info";
 import svgLoader from "vite-svg-loader";
 import type { PluginOption } from "vite";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Inspector from "vite-plugin-vue-inspector";
 import { configCompressPlugin } from "./compress";
 import removeNoMatch from "vite-plugin-router-warn";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -13,7 +14,6 @@ import { themePreprocessorPlugin } from "@pureadmin/theme";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import { genScssMultipleScopeVars } from "../src/layout/theme";
 import { vitePluginFakeServer } from "vite-plugin-fake-server";
-import Inspector from "vite-plugin-vue-inspector";
 
 export function getPluginsList(
   VITE_CDN: boolean,
@@ -28,9 +28,9 @@ export function getPluginsList(
       jitCompilation: false,
       include: [pathResolve("../locales/**")]
     }),
-    viteBuildInfo(),
-    // 选中DOM 打开IDE
+    // 按下Command(⌘)+Shift(⇧)，然后点击页面元素会自动打开本地IDE并跳转到对应的代码位置
     Inspector(),
+    viteBuildInfo(),
     /**
      * 开发环境下移除非必要的vue-router动态路由警告No match found for location with path
      * 非必要具体看 https://github.com/vuejs/router/issues/521 和 https://github.com/vuejs/router/issues/359
