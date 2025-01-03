551292078e09ca273615de6ce8b70c8a3fffb99d
xiaoming <1923740402@qq.com>
2025-01-04 02:19:38 +0800

    fix: 修复`aria-hidden`报错 (#1192)

--- src/assets/iconfont/iconfont.js
+++ src/assets/iconfont/iconfont.js
@@ -28,8 +28,7 @@
           c = document.createElement("div");
         (c.innerHTML = e._iconfont_svg_string_2208059),
           (c = c.getElementsByTagName("svg")[0]) &&
-            (c.setAttribute("aria-hidden", "true"),
-            (c.style.position = "absolute"),
+            ((c.style.position = "absolute"),
             (c.style.width = 0),
             (c.style.height = 0),
             (c.style.overflow = "hidden"),
