fa915fe4c36008a39aa8d7d30a34dc394209c1a2
xiaoxian521 <1923740402@qq.com>
2025-03-24 13:33:18 +0800

    chore: update

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import "animate.css";
-// 引入 src/components/ReIcon/src/offlineIcon.ts 文件中所有使用addCollection添加过的本地图标
+// 引入 src/components/ReIcon/src/offlineIcon.ts 文件中所有使用addIcon添加过的本地图标
 import "@/components/ReIcon/src/offlineIcon";
 import { setType } from "./types";
 import { useI18n } from "vue-i18n";
