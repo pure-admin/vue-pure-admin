fe7d67af0d4e7ffdf25aa8afb00a8da43332e060
xiaoxian521 <1923740402@qq.com>
2025-05-27 14:30:44 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ import en from "element-plus/es/locale/lang/en";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import plusEn from "plus-pro-components/es/locale/lang/en";
 import plusZhCn from "plus-pro-components/es/locale/lang/zh-cn";
-// import { ElNotification } from "element-plus";
+import { ElNotification } from "element-plus";
 
 export default defineComponent({
   name: "app",
@@ -55,23 +55,20 @@ export default defineComponent({
       );
     }
   },
-  // mounted() {
-  //   ElNotification({
-  //     title: "限时活动",
-  //     duration: 0,
-  //     // @ts-expect-error
-  //     style: { width: "200px" },
-  //     position: "bottom-right",
-  //     dangerouslyUseHTMLString: true,
-  //     message: `
-  //       <a target='_blank' style='font-size: 16px' class='block text-center border mb-2 rounded hover:text-[red]' href='https://pure-admin.cn/pages/js/'>
-  //       JS版本
-  //       </a>
-  //       <a target='_blank' style='font-size: 16px' class='block text-center border mb-2 rounded hover:text-[red]' href='https://pure-admin.cn/pages/max/'>
-  //       Max版本
-  //       </a>
-  //       `
-  //   });
-  // }
+  mounted() {
+    ElNotification({
+      title: "高级服务",
+      duration: 0,
+      // @ts-expect-error
+      style: { width: "200px" },
+      position: "bottom-right",
+      dangerouslyUseHTMLString: true,
+      message: `
+        <a target='_blank' class='block text-base text-center border mt-2 rounded hover:text-[red]!' href='https://pure-admin.cn/pages/service/'>
+        点我查看
+        </a>
+      `
+    });
+  }
 });
 </script>
