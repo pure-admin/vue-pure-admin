e7e90f970149970bdde939ba359d044c3d93d9f9
xiaoxian521 <1923740402@qq.com>
2025-09-17 14:11:22 +0800

    feat: 添加全屏`403`、`404`、`500`页面，全屏错误页面清晰且安全，提升用户体验

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -12,13 +12,46 @@ export default [
       rank: 101
     }
   },
+  // 全屏403（无权访问）页面
+  {
+    path: "/access-denied",
+    name: "AccessDenied",
+    component: () => import("@/views/error/403.vue"),
+    meta: {
+      title: $t("menus.pureFourZeroOne"),
+      showLink: false,
+      rank: 102
+    }
+  },
+  // 全屏404（页面不存在）页面
+  {
+    path: "/:pathMatch(.*)*",
+    name: "PageNotFound",
+    component: () => import("@/views/error/404.vue"),
+    meta: {
+      title: $t("menus.pureFourZeroFour"),
+      showLink: false,
+      rank: 103
+    }
+  },
+  // 全屏500（服务器出错）页面
+  {
+    path: "/server-error",
+    name: "ServerError",
+    component: () => import("@/views/error/500.vue"),
+    meta: {
+      title: $t("menus.pureFive"),
+      showLink: false,
+      rank: 104
+    }
+  },
   {
     path: "/redirect",
     component: Layout,
     meta: {
       title: $t("status.pureLoad"),
       showLink: false,
-      rank: 102
+      rank: 105
     },
     children: [
       {
@@ -28,6 +61,16 @@ export default [
       }
     ]
   },
+  {
+    path: "/account-settings",
+    name: "AccountSettings",
+    component: () => import("@/views/account-settings/index.vue"),
+    meta: {
+      title: $t("buttons.pureAccountSettings"),
+      showLink: false,
+      rank: 106
+    }
+  },
   // 下面是一个无layout菜单的例子（一个全屏空白页面），因为这种情况极少发生，所以只需要在前端配置即可（配置路径：src/router/modules/remaining.ts）
   {
     path: "/empty",
@@ -36,17 +79,7 @@ export default [
     meta: {
       title: $t("menus.pureEmpty"),
       showLink: false,
-      rank: 103
-    }
-  },
-  {
-    path: "/account-settings",
-    name: "AccountSettings",
-    component: () => import("@/views/account-settings/index.vue"),
-    meta: {
-      title: $t("buttons.pureAccountSettings"),
-      showLink: false,
-      rank: 104
+      rank: 107
     }
   }
 ] satisfies Array<RouteConfigsTable>;
