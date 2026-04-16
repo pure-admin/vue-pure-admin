9aaaadf1adcefe94a1ded4425f367a3d4f8ee1ac
xiaoxian521 <1923740402@qq.com>
2026-04-16 09:24:58 +0800

    feat: `useRenderIcon`通用图标函数支持渲染`SVG`字符串和图片`URL`

--- src/components/ReIcon/src/hooks.ts
+++ src/components/ReIcon/src/hooks.ts
@@ -2,25 +2,56 @@ import type { iconType } from "./types";
 import { h, defineComponent, type Component } from "vue";
 import { FontIcon, IconifyIconOnline, IconifyIconOffline } from "../index";
 
+const ifReg = /^IF-/;
+const svgReg = /^\s*<svg[\s>]/;
+const svgCache = new Map<string, string>();
+const imgReg = /^(https?:\/\/|\/\/|data:image\/)/;
+
 /**
- * 支持 `iconfont`、自定义 `svg` 以及 `iconify` 中所有的图标
+ * 支持 `iconfont`、`SVG` 字符串、`SVG` 函数组件、图片 `URL` 以及 `iconify` 中所有的图标
  * @see 点击查看文档图标篇 {@link https://pure-admin.cn/pages/icon/}
  * @param icon 必传 图标
  * @param attrs 可选 iconType 属性
  * @returns Component
  */
 export function useRenderIcon(icon: any, attrs?: iconType): Component {
-  // iconfont
-  const ifReg = /^IF-/;
-  // typeof icon === "function" 属于SVG
-  if (ifReg.test(icon)) {
+  if (typeof icon === "string" && svgReg.test(icon)) {
+    // SVG 字符串
+    let cleanedSvg = svgCache.get(icon);
+    if (cleanedSvg === undefined) {
+      if (svgCache.size > 200) svgCache.clear();
+      cleanedSvg = icon.replace(
+        /<svg([^>]*)>/,
+        (_, a) => `<svg${a.replace(/\s*(width|height)="[^"]*"/g, "")}>`
+      );
+      svgCache.set(icon, cleanedSvg);
+    }
+    return defineComponent({
+      name: "SvgRawIcon",
+      render: () =>
+        h("span", {
+          class: "svg-raw-icon",
+          innerHTML: cleanedSvg,
+          ...attrs
+        })
+    });
+  } else if (typeof icon === "string" && imgReg.test(icon)) {
+    // 图片 URL
+    return defineComponent({
+      name: "ImgIcon",
+      render: () =>
+        h("img", {
+          src: icon,
+          style: { width: "18px", height: "18px", objectFit: "contain" },
+          ...attrs
+        })
+    });
+  } else if (ifReg.test(icon)) {
     // iconfont
     const name = icon.split(ifReg)[1];
-    const iconName = name.slice(
-      0,
-      name.indexOf(" ") == -1 ? name.length : name.indexOf(" ")
-    );
-    const iconType = name.slice(name.indexOf(" ") + 1, name.length);
+    const spaceIdx = name.indexOf(" ");
+    const iconName = spaceIdx === -1 ? name : name.slice(0, spaceIdx);
+    const iconType = spaceIdx === -1 ? name : name.slice(spaceIdx + 1);
     return defineComponent({
       name: "FontIcon",
       render() {
@@ -32,7 +63,7 @@ export function useRenderIcon(icon: any, attrs?: iconType): Component {
       }
     });
   } else if (typeof icon === "function" || typeof icon?.render === "function") {
-    // svg
+    // SVG 函数组件
     return attrs ? h(icon, { ...attrs }) : icon;
   } else if (typeof icon === "object") {
     return defineComponent({
